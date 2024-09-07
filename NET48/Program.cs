72a85fd7543f38629559be953324ba382662e034
zufuliu <zufuliu@gmail.com>
2024-09-07 12:39:01 +0800

    Display unhandled exception.

--- NET48/Program.cs
+++ NET48/Program.cs
@@ -10,7 +10,13 @@ namespace FindInFiles {
 		static void Main(string[] args) {
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
+			AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 			Application.Run(new FindInFilesForm(args));
+		}
+
+		private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
+			var exc = e.ExceptionObject as Exception;
+			MessageBox.Show(exc?.StackTrace, exc?.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -8,7 +8,13 @@ namespace FindInFiles {
 			// To customize application configuration such as set high DPI settings or default font,
 			// see https://aka.ms/applicationconfiguration.
 			ApplicationConfiguration.Initialize();
+			AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 			Application.Run(new FindInFilesForm(args));
+		}
+
+		private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
+			var exc = e.ExceptionObject as Exception;
+			MessageBox.Show(exc?.StackTrace, exc?.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
 		}
 	}
 }
